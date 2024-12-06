f71c6df07df82cb12cd21a4f8590ae3a6168eb3c
Abel Yohanes <k9ja1xwk@students.codeinstitute.net>
2024-12-06 13:22:18 +0000

    test with array content was conducted and passed

--- scripts/game.test.js
+++ scripts/game.test.js
@@ -24,4 +24,7 @@ describe("game object contains correct keys", () => {
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
     });
+    test("choices contains correct Ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
 });
